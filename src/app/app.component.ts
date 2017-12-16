357f532d935a5c797961952e1e71707638ae2fa9
dhonisaputra <dhoni.p.saputra@gmail.com>
2017-12-17 02:28:56 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'history', component: HistoryPage },
+      { title: 'history', component: HistoryPage }, 
       { title: 'settings', component: SettingsPage }
     ];
 

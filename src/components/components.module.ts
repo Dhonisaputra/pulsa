b66b652ba315cf0e01c62aec6dda1ee7815e7fac
dhonisaputra <dhoni.p.saputra@gmail.com>
2017-12-17 02:26:39 +0700

    first commit

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { HistoryComponent } from './history/history';
+@NgModule({
+	declarations: [HistoryComponent],
+	imports: [],
+	exports: [HistoryComponent]
+})
+export class ComponentsModule {}
